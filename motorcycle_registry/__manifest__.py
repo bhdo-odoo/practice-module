2c991de020b7782ad80cdf95f429681eb1ae87f0
“bhdo-odoo” <bhdo@odoo.com>
2023-10-12 10:37:20 +0530

    Initial Commit

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name' : 'Motorcycle Registry',
+    'summary' : 'Manage Registration of Motorcycles',
+    'description' : '''
+        Motorcycle Registry
+        ====================
+        This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycle of the brand.
+    ''',
+    'author' : 'bhdo-odoo',
+    'version' : '0.0.1',
+    'category' : 'Kawiil/Custom Modules',
+    'website' : 'https://github.com/kro-odoo/batch10-practice-module/tree/16.0-motorcycle-registry-bhdo',
+    'application' : True,
+    'installable' : True,
+    'license' : 'LGPL-3'
+}
