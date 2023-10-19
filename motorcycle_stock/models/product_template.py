2da3bb7159f021ec04a1c4daf750b6928cbdec7c
“bhdo-odoo” <bhdo@odoo.com>
2023-10-19 18:55:39 +0530

    Bonus Modules 1&2 in progress

--- motorcycle_stock/models/product_template.py
+++ motorcycle_stock/models/product_template.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ProductTemplateInherit(models.Model):
     _inherit = 'product.template'
@@ -21,3 +21,41 @@ class ProductTemplateInherit(models.Model):
     model = fields.Char(string='Model')
     year = fields.Char(string='Year')
     launch_date = fields.Date(string='Launch Date')
+
+    @api.model
+    def create(self, values):
+        if values.get('type') == 'product' and values.get('product_variant_count', 0) == 1:
+            product_variant = self.env['product.product'].create({
+                'year': values.get('year'),
+                'make': values.get('make'),
+                'model': values.get('model'),
+                'detailed_type': values.get('detailed_type'),
+            })
+            if product_variant.detailed_type == 'motorcycle':
+                values['name'] = f"{product_variant.year} {product_variant.make} {product_variant.model}"
+
+        product_template = super(ProductTemplateInherit, self).create(values)
+
+        return product_template
+
+
+
+    # @api.depends('name', 'make', 'model', 'year', 'detailed_type')
+    # def _compute_motorcycle_name(self):
+    #     for template in self: 
+    #         if template.type == 'product' and template.product_variant_count == 1:
+    #             variant = template.product_variant_ids
+    #             if variant.detailed_type == 'motorcycle':
+    #                 template.name = f"{variant.year}{variant.make}{variant.model}"
+    #             else:
+    #                 template.name = template.name
+    #         else:
+    #             template.name = template.name
+        # for template in self:
+        #     variant = template.product_variant_ids
+        #     if variant.detailed_type != 'motorcycle':
+        #         return variant.name
+        #     elif variant.detailed_type == 'motorcycle':
+        #             template.name = f"{variant.make}{variant.model}{variant.year}"
+        #     else:
+        #         template.name = ""
