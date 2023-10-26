a7d7c4e71986612535503a39847ab92130f0b7b6
“bhdo-odoo” <bhdo@odoo.com>
2023-10-26 16:06:18 +0530

    Made the code according to the coding guidelines

--- motorcycle_stock/models/product_lot.py
+++ motorcycle_stock/models/product_lot.py
@@ -5,13 +5,13 @@ class StockLotInherit(models.Model):
 
     name = fields.Char(compute='_compute_name_lot', store=True, readonly=False)
 
-    @api.depends('product_id.product_tmpl_id.detailed_type', 'product_id.product_tmpl_id.year', 
-                'product_id.product_tmpl_id.make', 'product_id.product_tmpl_id.model', 'product_id.product_tmpl_id.battery_capacity', 'name')
+    @api.depends('product_id.detailed_type', 'product_id.year', 
+                'product_id.make', 'product_id.model', 'product_id.battery_capacity', 'name')
     def _compute_name_lot(self):
         for lot in self:
-            if lot.product_id.product_tmpl_id.detailed_type == 'motorcycle':
-                name_parts = [part for part in [lot.product_id.product_tmpl_id.year, lot.product_id.product_tmpl_id.make, 
-                                                lot.product_id.product_tmpl_id.model, lot.product_id.product_tmpl_id.battery_capacity] if part]
+            if lot.product_id.detailed_type == 'motorcycle':
+                name_parts = [part for part in [lot.product_id.year, lot.product_id.make, 
+                                                lot.product_id.model, lot.product_id.battery_capacity] if part]
                 lot.name = ''.join(name_parts)
             else:
                 lot.name = lot.name
